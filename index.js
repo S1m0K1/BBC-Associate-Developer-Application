d3ee5d5dd099e431ac91d1cd1052283ee38d6ac3
Simone Klein <simoneandpaper@gmail.com>
2020-05-14 12:29:07 +0100

    Demonstrate reverseString function for user input

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
 
+document.getElementById("demo").addEventListener("change", reverseString);
+
 function reverseString() {
-    let input = document.querySelector("h1").innerHTML;
-    
-    let stringArray = input.split("");
-    let reverseArray = stringArray.reverse();
-    let joinString = reverseArray.join("");
-    
-    return input = joinString;       
+    let input = document.getElementById("demo");
+    input.value = input.value.split("").reverse().join("");
 }
 
+// function reverseString() {
+//     let input = document.querySelector("h1").innerHTML;
 
-// let input = prompt("Enter a word or sentence").toLowerCase();
-
-// function reverseString(input) {
 //     let stringArray = input.split("");
 //     let reverseArray = stringArray.reverse();
 //     let joinString = reverseArray.join("");
 
-//     return joinString;
+//     return input = joinString;       
 // }
-
-// let input = prompt("Enter a word or sentence");
-// console.log(input);
-
-// // Convert input to lower case 
-// let lowerString = input.toLowerCase();
-// console.log(lowerString);
-
-// // 1. Use split() method to make a new array
-// let stringArray = lowerString.split("");
-// console.log(stringArray);
-
-// // 2. Use reverse() method to reverse the new array
-// let reverseArray = stringArray.reverse();
-// console.log(reverseArray);
-
-// // 3. Use join() method to join reverseArray into a string
-// let reverseString = reverseArray.join("");
-// console.log(reverseString);
