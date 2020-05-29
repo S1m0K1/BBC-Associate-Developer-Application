1ca049feeab8cab61037a9fa1e2d62274bf7dd48
Simone Klein <simoneandpaper@gmail.com>
2020-05-29 12:11:21 +0100

    Work through solution to shorten input ...
    Pixielabs Office Hours
    
    Also consider how to reverse words

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # BBC-Associate-Developer-Application
+
+A simple algorithm to reverse letters in place of a user-input word or sentence.
+<br /> 
+
+The hosted version is available here:
+<br /> 
+
+<br />
+
+## Technology Used
+
+It uses the following technology:
+<br />
+* JavaScript (ES2015+)
+* CSS
+* HTML
--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-
 document.getElementById("demo").addEventListener("change", reverseString);
 
 function reverseString() {
     let input = document.getElementById("demo");
-    input.value = input.value.split("").reverse().join("");
+    
+    // 1. Shorten the user input to 12 characters ... 
+    // Get the value of the input i.e. a string -> substring() method only works on a string:
+    let inputString = input.value;
+    let shortString = inputString.substring(0, 11) + " ...";
+
+    // 2. Reverse the letters in place:
+    input.value = shortString.split("").reverse().join("");
 }
 
-// function reverseString() {
-//     let input = document.querySelector("h1").innerHTML;
+// To reverse the words:
 
+// function reverseString() {
+//     let input = document.getElementById("demo");
+    // Splits at the spaces " ", joins with spaces " "
+//     input.value = input.value.split(" ").reverse().join(" ");
+// }
+   
 //     let stringArray = input.split("");
 //     let reverseArray = stringArray.reverse();
 //     let joinString = reverseArray.join("");
